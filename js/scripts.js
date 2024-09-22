8047faf2dfc4fc70767e6e226760edccccaf2d40
milad <miladghalejughi@gmail.com>
2024-09-22 16:46:57 +1000

    add calender event

--- js/scripts.js
+++ js/scripts.js
@@ -14,10 +14,43 @@ document.addEventListener('DOMContentLoaded', function() {
         musicSection.style.display = 'none';
     });
 
+    const savedEvents = JSON.parse(localStorage.getItem('events')) || [];
     calendarLink.addEventListener('click', function() {
         homeSection.style.display = 'none';
         calendarSection.style.display = 'block';
         musicSection.style.display = 'none';
+            selectable: true,
+            events: savedEvents,
+            eventClick: function(info) {
+                const action = prompt('Enter "edit" to edit or "delete" to delete this event:');
+                if (action === 'edit') {
+                    let newTitle = prompt('Enter new title:', info.event.title);
+                    if (newTitle) {
+                        info.event.setProp('title', newTitle);
+                        updateLocalStorage();
+                    }
+                } else if (action === 'delete') {
+                    info.event.remove();
+                    updateLocalStorage();
+                }
+            },
+            select: function(info) {
+                let title = prompt('Enter event title:');
+                if (title) {
+                    const event = {
+                        title: title,
+                        start: info.startStr,
+                        end: info.endStr,
+                        allDay: info.allDay
+                    };
+                    calendar.addEvent(event);
+                    savedEvents.push(event);
+                    localStorage.setItem('events', JSON.stringify(savedEvents));
+                }
+                calendar.unselect();
+            }
+        });
+        calendar.render();
     });
 
     musicLink.addEventListener('click', function() {
